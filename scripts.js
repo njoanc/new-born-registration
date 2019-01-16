3151a50db727eee400d3031e8a7f911e493a84fa
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-01-16 10:42:45 +0200

    fixing conflict

---
+++ scripts.js
@@ -0,0 +1,21 @@
+//Business Logic: Contact and Address Constructots
+function Contact(
+  first,
+  last,
+  gender,
+  birth,
+  father,
+  mother,
+  acceptancy,
+  disabilities
+) {
+  this.firstName = first;
+  this.lastName = last;
+  this.gender = gender;
+  this.birthDay = birth;
+  this.fatherName = father;
+  this.motherName = mother;
+  this.childAcceptancy = acceptancy;
+  this.disabilities = disabilities;
+  this.addresses = addresses;
+}
